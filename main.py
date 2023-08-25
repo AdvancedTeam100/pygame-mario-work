9feda505c9d4e98214fd21e7dc7c1db7a55d21a6
AdvancedTeam100 <131359924+AdvancedTeam100@users.noreply.github.com>
2023-08-25 09:49:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,6 @@
 #If you want, I'll pay directly. First I will pay 30$ by crypto and if you complete the project I will pay other 70$.
 
 #What do you think about this?
+
+# this is my skype id    live:.cid.ce029220ae99ae97
+# please send you crypto address so that I can send a money.
